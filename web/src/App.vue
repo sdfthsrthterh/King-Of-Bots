59400d1121fc6ae40ca228e4443faa15fab1b150
sdfthsrthterh <1410726455@qq.com>
2025-03-29 22:14:18 +0800

    database connection login and logout

--- web/src/App.vue
+++ web/src/App.vue
@@ -8,9 +8,54 @@ import NavBar from "@/components/NavBar.vue"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
 
+
 export default {
   components: {
     NavBar
+  },
+  setup() {
+    // $.ajax({
+    //   url: "http://localhost:3000/user/account/token/",
+    //   type: "post",
+    //   data: {
+    //     username: 'yxc',
+    //     password: "pyxc",
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // });
+    // $.ajax({
+    //   url:"http://localhost:3000/user/account/info/",
+    //   type: "get",
+    //   headers: {
+    //     Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJmZWVhNjRmMmJiNTA0Y2RiYmU3OWQ0MTc5NWE3MWJlYSIsInN1YiI6IjIiLCJpc3MiOiJzZyIsImlhdCI6MTc0MzE2NTYxMCwiZXhwIjoxNzQ0Mzc1MjEwfQ.VNcTmq5HEGlLXbeGi0c-b2T2KiMAbKYGBxd8WGphP0o",
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // }),
+    // $.ajax({
+    //   url: "http://localhost:3000/user/account/register/",
+    //   type: "post",
+    //   data: {
+    //     username: "yxc5",
+    //     password: "123",
+    //     confirmedPassword: "123",
+    //   },
+    //   success(resp) {
+    //     console.log(resp);
+    //   },
+    //   error(resp) {
+    //     console.log(resp);
+    //   }
+    // })
   }
 }
 </script>
